0d0c3bf110f7382b1854615d27cda1f4d2a7a38c
John Mwaura <johnmwaura08@gmail.com>
2021-04-17 11:29:38 -0500

    using indexof

---
+++ indexof.js
@@ -0,0 +1,8 @@
+/** the indexOf method returns the first index of which a given item
+ * can be found inside an array
+ *
+ */
+
+const majina = ["Jonte", "Tony", "Gilly", "Laura"];
+
+console.log(majina.indexOf("Laura"));
